f9f1aa5af2766816fc7c7f0c7ffe33d23918c9dd
HYUNSOOSHIN <shs0655@gmail.com>
2021-02-21 17:22:32 +0900

    21/02/21 환경변수 설정 수정

--- src/config.js
+++ src/config.js
@@ -1,3 +1,14 @@
+const type = "dev"
+
+const URL = {
+  prod: {
+    API_URL: "prod_api_url",
+  },
+  dev: {
+    API_URL: "dev_api_url",
+  },
+}
+
 module.exports = {
-  API_URL: process.env.API_URL || "api url",
+  API_URL: process.env.API_URL || URL[type].API_URL,
 }
