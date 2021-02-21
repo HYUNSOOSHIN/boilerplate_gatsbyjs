8d409733ed90a172420e41a913d99b8954f1d8ad
HYUNSOOSHIN <shs0655@gmail.com>
2021-02-21 17:19:12 +0900

    21/02/21 redux-persist - storage 변경 ( local > session )

--- src/store/index.js
+++ src/store/index.js
@@ -3,14 +3,15 @@ import { createLogger } from "redux-logger"
 import ReduxThunk from "redux-thunk"
 import penderMiddleware from "redux-pender"
 import { persistReducer } from "redux-persist"
-import storage from "redux-persist/lib/storage"
+// import storage from "redux-persist/lib/storage" // localStorage
+import storageSession from "redux-persist/lib/storage/session" // sessionStorage
 import reducers from "../reducers"
 
 const logger = createLogger()
 
 const persistConfig = {
   key: "root",
-  storage,
+  storage: storageSession,
 }
 
 const enhancedReducer = persistReducer(persistConfig, reducers)
